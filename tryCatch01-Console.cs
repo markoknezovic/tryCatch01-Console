201fb2d1cc90f12ed4e2850a6b6002faae663258
markoknezovic <58472152+markoknezovic@users.noreply.github.com>
2020-11-16 09:02:41 +0100

    Add files via upload

---
+++ tryCatch01-Console.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch01_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj;
+            Console.Write("unesi broj: ");
+            try
+            {
+                broj = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(broj);
+            }
+            catch (Exception greska)
+            {
+                Console.WriteLine(greska.Message);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
